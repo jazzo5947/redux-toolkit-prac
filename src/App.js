09a02e94c63b33fe971f6df6aa3003cb3cda73ba
jazzo <@jazzo5947@gmail.com>
2023-06-21 23:04:24 +0900

    feat: redux toolkit first try!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createStore } from "redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import store from "./store";
+import { up } from "./counterSlice";
 
-function App() {
+// function reducer(state, action) {
+//   if (action.type === "up") {
+//     return { ...state, value: state.value + action.step };
+//   }
+
+//   return state;
+// }
+
+// const initialState = { value: 0 };
+// const store = createStore(reducer, initialState);
+
+function Counter() {
+  const count = useSelector((state) => {
+    return state.counter.value;
+  });
+
+  const dispatch = useDispatch();
+  console.log(count);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          // dispatch({ type: "counterSlice/up", step: 2 });
+          dispatch(up(2));
+        }}
+      >
+        +
+      </button>
+      {count}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter></Counter>
+      </div>
+    </Provider>
+  );
+}
